366eb78db0ecb79ed6d3254ef5d79c262c9de244
Torrenceh98 <torrenceh98@gmail.com>
2018-10-27 13:33:09 -0500

    pst page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Home from "./pages/Home"
+import Post from "./pages/Blog"
 
 const App = () =>
   <Router>
@@ -11,7 +12,7 @@ const App = () =>
         <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/newjob" component={} />
+        <Route exact path="/newjob" component={Post} />
         {/* <Route exact path="/search" component={Search} /> */}
         {/* <Route component={NoMatch} /> */}
       </Switch>
