26ea26be8b9b19c220db20e86cedf71152e7ba01
Torrenceh98 <torrenceh98@gmail.com>
2018-11-05 21:19:46 -0600

    pull

--- routes/api/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userController = require("../../controllers/userController")
-
-router.route("/create")
-    .post(userController.create)    
-
-module.exports= router
